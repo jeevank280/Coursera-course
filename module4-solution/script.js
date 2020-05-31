8341f89c8c139bcea071573a6a521819130b42d5
jeevank280 <jeevankori280@gmail.com>
2020-05-31 22:37:12 +0530

    first commit

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,20 +1,20 @@
 /*
 Solution of assignment 4:
 Expected output:
-Goodbye Jeevan
-Goodbye John
-Hello Yogesh
-Goodbye Jason
-Hello Tony
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
 Hello Frank
 Hello Larry
 Hello Paula
-Hello Kevin
-Goodbye Jim
+Hello Laura
+Good Bye Jim
 */
 
 (function() {
-    var names = ["Jeevan", "John", "Yogesh", "Jason", "Tony", "Frank", "Larry", "Paula", "Kevin", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
