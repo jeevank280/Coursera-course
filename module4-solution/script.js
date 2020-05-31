a5ecdd13a3e05d77ab0af698d1439c4a5f72b61d
jeevank280 <jeevankori280@gmail.com>
2020-05-31 22:26:05 +0530

    Second commit

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,16 +1,16 @@
 /*
 Solution of assignment 4:
 Expected output:
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
+Goodbye Jeevan
+Goodbye John
+Hello Yogesh
+Goodbye Jason
+Hello Tony
 Hello Frank
 Hello Larry
 Hello Paula
-Hello Laura
-Good Bye Jim
+Hello Kevin
+Goodbye Jim
 */
 
 (function() {
